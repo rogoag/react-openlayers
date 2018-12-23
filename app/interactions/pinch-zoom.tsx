6ad617044e4d850168527df9436383672997890d
SirMcPotato <vincent.samblas@gmail.com>
2018-12-23 18:54:25 +0100

    Add PinchRotate example

--- app/interactions/pinch-zoom.tsx
+++ app/interactions/pinch-zoom.tsx
@@ -7,11 +7,32 @@ import {
   Map, Layers, Overlay, Util    //objects
 } from "react-openlayers";
 
+import Highlighter from "../Highlighter";
+
 export class PinchZoom extends React.Component<any, any> {
   render() {
     return (
-      <div className="todo">
-        TODO: Coming Soon(PR would be highly appreciated)
+      <div>
+        <p>This example have disabled default <code>PinchZoom</code> interaction to provide one that zoom to the closest integer zoom level after gesture ends.</p>
+        <Map view={{ center: [0, 0], zoom: 2 }}>
+          <Layers>
+            <layer.Tile />
+          </Layers>
+          <Interactions pinchRotate={false} pinchZoom={false}>
+            <interaction.PinchZoom constrainResolution={true}/>
+          </Interactions>
+        </Map>
+        <Highlighter lang="jsx" code={
+`<Map view={{ center: [0, 0], zoom: 2 }}>
+  <Layers>
+    <layer.Tile />
+  </Layers>
+  <Interactions pinchRotate={false} pinchZoom={false}>
+    <interaction.PinchZoom constrainResolution={true}/>
+  </Interactions>
+</Map>`
+        } />
+        <a href="https://github.com/allenhwkim/react-openlayers/blob/master/app/interactions/pinch-zoom.tsx">source</a>
       </div>
     );
   }
